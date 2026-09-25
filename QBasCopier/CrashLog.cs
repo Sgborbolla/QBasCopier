72effb5d99b4533489d280feb90f3d6607f58d2a
Sgborbolla <sgborbolla@gmail.com>
2026-09-25 00:31:37 -0400

    CrashLog: guardar stack completo en Descargas (MediaStore) + dialogo con detalle

--- QBasCopier/CrashLog.cs
+++ QBasCopier/CrashLog.cs
@@ -10,23 +10,48 @@ public static class CrashLog
 
     static CrashLog()
     {
-        AppDomain.CurrentDomain.UnhandledException += (_, e) => Save("UNHANDLED " + (e.ExceptionObject?.ToString() ?? "?"));
-        System.Threading.Tasks.TaskScheduler.UnobservedTaskException += (_, e) => Save("TASK " + e.Exception);
+        AppDomain.CurrentDomain.UnhandledException += (_, e) => Report("UNHANDLED " + (e.ExceptionObject?.ToString() ?? "?"));
+        System.Threading.Tasks.TaskScheduler.UnobservedTaskException += (_, e) => Report("TASK " + e.Exception);
 #if ANDROID
-        global::Android.Runtime.AndroidEnvironment.UnhandledExceptionRaiser += (_, e) => Save("JAVA " + e.Exception);
+        global::Android.Runtime.AndroidEnvironment.UnhandledExceptionRaiser += (s, e) =>
+        {
+            var m = "JAVA " + e.Exception;
+            Report(m);
+        };
 #endif
     }
 
     public static void Info(string msg) => Save("INFO " + msg);
 
-    public static void Save(string msg)
+    private static void Report(string msg)
+    {
+        Save(msg);
+        ShowDialog(msg);
+    }
+
+    private static void ShowDialog(string msg)
+    {
+        try
+        {
+#if ANDROID
+            global::Android.App.AlertDialog.Builder b = new(global::Android.App.Application.Context);
+            b.SetTitle("QBasCopier - error");
+            b.SetMessage(msg.Length > 6000 ? msg[..6000] : msg);
+            b.SetPositiveButton("OK", (s, e) => { });
+            b.Show();
+#endif
+        }
+        catch { }
+    }
+
+    private static void Save(string msg)
     {
         try
         {
             lock (Lock)
             {
-                var line = DateTime.Now.ToString("HH:mm:ss ") + msg + "\n";
-                Console.Error.Write(line);
+                var full = DateTime.Now.ToString("HH:mm:ss ") + msg + "\n";
+                Console.Error.WriteLine("QBasCrash " + full);
 #if ANDROID
                 try
                 {
@@ -37,21 +62,29 @@ public static class CrashLog
                         var dir = new Java.IO.File(cd, "log");
                         if (!dir.Exists()) dir.Mkdirs();
                         _file ??= Path.Combine(dir.AbsolutePath, "crash.txt");
-                        File.AppendAllText(_file, line);
+                        File.AppendAllText(_file, full);
                     }
                 }
                 catch { }
                 try
                 {
-                    var m = msg.Length > 160 ? msg[..160] : msg;
-                    global::Android.Widget.Toast.MakeText(global::Android.App.Application.Context, m, global::Android.Widget.ToastLength.Long)?.Show();
+                    var cr = global::Android.App.Application.Context.ContentResolver!;
+                    var col = new global::Android.Content.ContentValues();
+                    col.Put(global::Android.Provider.MediaStore.MediaColumns.DisplayName, "QBasCopier-crash.txt");
+                    col.Put(global::Android.Provider.MediaStore.MediaColumns.MimeType, "text/plain");
+                    col.Put(global::Android.Provider.MediaStore.MediaColumns.RelativePath, "Download");
+                    var uri = cr.Insert(global::Android.Provider.MediaStore.Downloads.ExternalContentUri, col);
+                    if (uri != null)
+                        using (var os = cr.OpenOutputStream(uri))
+                        using (var w = new StreamWriter(os))
+                            w.Write(full);
                 }
                 catch { }
 #else
                 try
                 {
                     _file ??= Path.Combine(Path.GetTempPath(), "qbaswin-crash.log");
-                    File.AppendAllText(_file, line);
+                    File.AppendAllText(_file, full);
                 }
                 catch { }
 #endif
